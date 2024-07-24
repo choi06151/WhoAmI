5c8b2d4b3b5aa255e0b386d887f95457d98651c6
eunbin0123 <55692254+eunbin0123@users.noreply.github.com>
2024-07-24 15:51:04 +0900

    20240724
    - c++ 파일 생성

---
+++ Source/WhoAmI/WhoAmI.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "WhoAmI.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, WhoAmI, "WhoAmI" );
