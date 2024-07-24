86ed48d9f44ec6ddf0fe7c22ee6e90582fbc1d1d
eunbin0123 <55692254+eunbin0123@users.noreply.github.com>
2024-07-24 16:33:24 +0900

    20240724
    - c++ 파일 경로 수정

--- Source/WhoAmI/Private/SEB/DoorActor.cpp
+++ Source/WhoAmI/Private/SEB/DoorActor.cpp
@@ -1,10 +1,10 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 
-#include "CrystalActor.h"
+#include "SEB/DoorActor.h"
 
 // Sets default values
-ACrystalActor::ACrystalActor()
+ADoorActor::ADoorActor()
 {
  	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
@@ -12,14 +12,14 @@ ACrystalActor::ACrystalActor()
 }
 
 // Called when the game starts or when spawned
-void ACrystalActor::BeginPlay()
+void ADoorActor::BeginPlay()
 {
 	Super::BeginPlay();
 	
 }
 
 // Called every frame
-void ACrystalActor::Tick(float DeltaTime)
+void ADoorActor::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
